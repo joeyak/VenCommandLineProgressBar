f1ba745a05ec6d9dd84442aae8f0f8e235f9e30f
joeyak <joeyak97@gmail.com>
2019-08-03 14:08:05 -0400

    Add code

--- Program.cs
+++ Program.cs
@@ -1,12 +1,19 @@
 ﻿using System;
+using System.Threading.Tasks;
 
 namespace VenCommandLineProgressBar
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var max = 100;
+            for (int i = 0; i < max+1; i++)
+            {
+                Console.Write($"[{new string('#', i) + new string('-', max - i)}] ({i/(double)max*100:00}%)\r");
+                await Task.Delay(100);
+            }
+            Console.WriteLine();
         }
     }
 }
